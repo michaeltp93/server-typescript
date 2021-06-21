2e97b19cea8b43f270e94b0b60d01f4a56ee24a1
michaeltp93 <michael.trejosp@hotmail.es>
2021-06-22 00:30:39 +0200

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async () => {
+  debugger;
+  return { hello: 'mariano' };
+});
+
+const port = process.env.PORT ?? 8000;
+const start = async () => {
+  try {
+    await fastify.listen(port, '0.0.0.0');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
